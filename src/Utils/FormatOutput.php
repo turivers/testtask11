725d1de985ee58ff5343c57aa7508148f830a1d7
Yaroslav <ya@turivers.com>
2019-08-12 22:32:50 +0000

    add out type for methods

--- src/Utils/FormatOutput.php
+++ src/Utils/FormatOutput.php
@@ -4,12 +4,12 @@ namespace App\Utils;
     
 class FormatOutput
 {
-    public function addTireSpaceBefore( string $c )
+    public function addTireSpaceBefore( string $c ): string
     {
         return '- ' . $c;
     }
 
-    public function boolToString( bool $c )
+    public function boolToString( bool $c ): string
     {
         return $c ? 'true' : 'false';
     }
